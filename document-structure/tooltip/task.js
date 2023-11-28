e72cf01100760fce3ed1b7f02c1f8f4c771b9f40
Alexey.K <lexxtlt@gmail.com>
2023-11-28 20:38:51 +0400

    внесены изменения

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -12,7 +12,7 @@ toolTips.forEach(function (tip) {
         let topCoordinate = tip.getBoundingClientRect().top < window.innerHeight / 2 ?  tip.getBoundingClientRect().bottom + 10 : tip.getBoundingClientRect().top - 30;
         let leftCoordinate = tip.getBoundingClientRect().left < window.innerWidth / 2 ? tip.getBoundingClientRect().left + 10 : tip.getBoundingClientRect().left - 10;
         let html = `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`;
-        if (tooltip && event.a[0].innerText == tooltipText) {
+        if (tooltip && event.target.innerText == tooltipText) {
             tooltip.classList.toggle('tooltip_active');
         } else if (tooltip) {
             tooltip.style.left = `${leftCoordinate}px`;
@@ -22,9 +22,8 @@ toolTips.forEach(function (tip) {
             tip.insertAdjacentHTML('afterend', html);
         };
         ;
-       
-        tooltipText = event.a[0].innerText;
-
+               
+        tooltipText = event.target.innerText
 
         return false;
     };
