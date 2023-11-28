40e94fcd29a4e57f61819b487e1f21ec0d639e9f
Alexey.K <lexxtlt@gmail.com>
2023-11-28 20:34:09 +0400

    внесены изменения

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,10 +1,11 @@
+'use strict'
 const inputTodo = document.getElementById("tasks__form");
 const tasksInput = document.getElementById("task__input");
 const taskList = document.getElementById("tasks__list");
 
-const removeTask = (e) => {
-	e.target.closest(".task").remove();
-}
+taskList.addEventListener("click", itemRemove)
+
+
 
 inputTodo.addEventListener('submit', addTask)
     
@@ -13,8 +14,9 @@ function addTask (event) {
     event.preventDefault();
 
     const taskText = tasksInput.value
-  
-
+    
+    if (taskText.trim() !== "") {
+      
     const taskHTML = `<div class="task">
     <div class="task__title">
       ${taskText}
@@ -26,11 +28,34 @@ function addTask (event) {
   
   tasksInput.value = "";
   tasksInput.focus();
-
-  [...(taskList.getElementsByClassName("task__remove"))].forEach(element => {
-    element.addEventListener("click", removeTask)
-});
 }
+
+}
+
+function itemRemove(e) {
+  const item = e.target;
+  
+  if (item.classList[0] === "task__remove") {
+      const todo = item.parentElement;
+      todo.remove();
+      
+    
+      }
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
